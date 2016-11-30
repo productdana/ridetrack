314eb229657ac67b2bf12dac0b9e9a3d32f4c5dc
Dana Tran <dana.l.tran@gmail.com>
2016-11-29 21:38:48 -0800

    add index.html, style.css, index.js, skeleton bones of index.html page

---
+++ client/index.js
@@ -0,0 +1,2 @@
+// var today = new Date();
+// document.getElementById('currentTime').innerHTML="Today's Date is :"+today;
--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const scheduleController = require('./schedule.js');
+const path = require('path');
 
 
 app.use(cors());
 
 app.get('/', function(req, res){
-	res.json('http://localhost:3000/schedule');
+	res.statusCode = 200;
+  res.contentType('text/html', 'charset=UTF-8');
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+  console.log(res.contentType);
+	// res.json('http://localhost:3000/schedule');
 });
 
 app.get('/schedule', scheduleController.getData);
