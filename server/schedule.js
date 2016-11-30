5bd2b95940ec8b11a54e88b24c20d85e5a27dac8
Dana Tran <dana.l.tran@gmail.com>
2016-11-29 17:38:02 -0800

    server API is complete but needs refactoring

--- server/schedule.js
+++ server/schedule.js
@@ -1,109 +1,158 @@
-const Promise = require('bluebird');
+// const Promise = require('bluebird');
 
-const scheduleController = {
-	getData: (req, res, next) => {
-    let busSchedule = [];
-    let route1Starts = [];
-    // var routeObj = {};
-    // routeObj.route1 = [];
-    // routeObj.route2 = [];
-    // routeObj.route3 = [];
-
+// const scheduleController = {
+// 	getData: (req, res, next) => {
+//     let busSchedule = [];
+//     let route1Starts = [];
     
-    //builds route1start array
-    function createRoute1Starts(){
-      for (let i = 0; i < 10; i++){
-        route1Starts.push(i*2);
-      }
-    }
+//     //builds route1start array
+//     function createRoute1Starts(){
+//       for (let i = 0; i < 10; i++){
+//         route1Starts.push(i*2);
+//       }
+//       return route1Starts;
+//     }
 
-    //creates an object for each stop, containing 3 routes per stop with the bus times for each route. stores each object for each stop in an busSchedule array
-    function createSchedule(){
+//     //get route times for 1 route for this 1 stop
+//     //add key of this route with value of [] to routeObj
+//     function getRoute(route, stop){
+//       var routeTimes = [];
+//       var route1Start = route1Starts[stop-1];
+//       var start;
+//       var routeTimesPromises = [];
+//       //since each route is 2 minutes apart, this determines what the first start time for that route is
+//       if (route === 1){
+//         start = route1Start;
+//       } else if (route === 2){
+//         start = route1Start + 2;
+//       } else {
+//         start = route1Start + 4;
+//       }
+
+//       // var threeRoutes = [];
+
+//       //1440 minutes in a day. increment i by 15 because a bus for that route arrives every 15 minutes
+//       for (let i = start; i < 1440; i+=15){
+//         routeTimesPromises.push(new Promise(function(resolve, reject){
+//           // if (err){
+//           //   reject(err);
+//           // };
+
+//           let hour = Math.floor(i / 60);
+//           if (hour < 1){
+//             hour = '00';
+//           } else if (hour < 10){
+//             hour = '0' + hour.toString();
+//           } else {
+//             hour = hour.toString();
+//           }
+//           let min = i % 60;
+//           if (min < 10){
+//             min = '0' + min.toString();
+//           } else {
+//             min = min.toString();
+//           }
+//           resolve(`${hour}:${min}`);
+//         }));
 
 
-      for (let i = 0; i < 10; i++){
-        var stopObj = {};
-        var y = i + 1;
-        stopObj[`stop${y}`] = routeObj;
-        // busSchedule.push('stop' + (i+1).toString()) = {};
-        busSchedule.push(stopObj);
-        // route1start.push(i*2);
-        getStop((i+1),route1start[i]);
-        //TODO: run getStop for each stop. need to populate an array of route1starts
-      }
-      // return busSchedule;
-      // console.log('NEW SECTION!!');
-      // console.log('route1start:', route1start);
-    }
-    
-    //get route times for 1 route for this 1 stop
-    //add key of this route with value of [] to routeObj
-    function getRoute(route, stop){
-      var routeTimes = [];
-      var route1Start = route1Starts[stop-1];
-      var start;
+//         // let hour = Math.floor(i / 60);
+//         // if (hour < 1){
+//         //   hour = '00';
+//         // } else if (hour < 10){
+//         //   hour = '0' + hour.toString();
+//         // } else {
+//         //   hour = hour.toString();
+//         // }
+//         // let min = i % 60;
+//         // if (min < 10){
+//         //   min = '0' + min.toString();
+//         // } else {
+//         //   min = min.toString();
+//         // }
 
-      //since each route is 2 minutes apart, this determines what the first start time for that route is
-      if (route === 1){
-        start = route1Start;
-      } else if (route === 2){
-        start = route1Start + 2;
-      } else {
-        start = route1Start + 4;
-      }
+//         // routeTimes.push(`${hour}:${min}`);
+//       }
+//       Promise.all(routeTimesPromises).then(function(results){
+//         console.log('results:', results);
+//         return results;
+//         // threeRoutes.push(results);
+//         // console.log('THREE ROUTES ARR:', threeRoutes);
+//         // var routeObj = {};
+//         // for (let i = 1; i <= threeRoutes.length; i++){
+//         //   routeObj[`route${i}`] = threeRoutes[i];
+//         // }
+//         // console.log('ROUTEOBJ:', routeObj);
+//         // return routeObj;
+//       }, function (err){
+//         console.log('err', err);
+//       })
+//       // return routeTimes;
+//       // routeObj[`route${route}`] = routeTimes;
+//     }
 
-      // console.log('HELLO. route:', route, 'stop:', stop, 'route1start:', route1start, 'other:', busSchedule[stop-1][`stop${stop}`][`route${route}`]);
-      // busSchedule[`stop${stop}`][`route${route}`] = [];
+//     //get bus times for all 3 routes for this stop
+//     //after all 3 route arrays are populated with bus times, add the routeObj as the value to that stop
+//     function getStop(stop){
+//       var threeRoutesPromises = [];
+//       //populate times for stop 1 route 1
 
-      //1440 minutes in a day. increment i by 15 because a bus for that route arrives every 15 minutes
-      for (let i = start; i < 1440; i+=15){
-        let hour = Math.floor(i / 60);
-        if (hour < 1){
-          hour = '00';
-        } else if (hour < 10){
-          hour = '0' + hour.toString();
-        } else {
-          hour = hour.toString();
-        }
-        let min = i % 60;
-        if (min < 10){
-          min = '0' + min.toString();
-        } else {
-          min = min.toString();
-        }
+//       // return new Promise(function(resolve, reject){
+//       //   if(error){
+//       //     reject(error);
+//       //   } else {
+//       //     resolve();
+//       //   }
+//       // })
+//       var stopObj = {};
+//       var routeObj = {};
+//       for (let i = 1; i < 4; i++){
+//         threeRoutesPromises.push(new Promise(function(resolve, reject){
+//           resolve(getRoute(i, stop));
+//           // routeObj[`route${i}`] = getRoute(i, stop);
+//           // resolve(routeObj);  
+//         }))
+//         // console.log('need to run getRoute(i)');
+        
+//         // getRoute(i, stop);
+//       }
+//       console.log('THREE ROUTES PROMISES:', threeRoutesPromises);
+//       Promise.all(threeRoutesPromises).then(function(results2){
+//         console.log('results2:', results2);
+//       })
+//       // console.log('*ROUTE OBJ:', routeObj);
+//       // return routeObj;
+//       // stopObj[`stop${stop}`] = routeObj;
+//       // busSchedule.push(stopObj);
+//       // console.log('BUS SCHEDULE:', busSchedule);
+//     } //end getStop
 
-        routeTimes.push(`${hour}:${min}`);
-          // busSchedule[`stop${stop}`][`route${route}`].push(`${hour}:${min}`);
-          // busSchedule[stop-1][`stop${stop}`][`route${route}`].push(`00:${j}`); //to use
-      }
-      routeObj[`route${route}`] = routeTimes;
-    }
+//     //creates an object for each stop, containing 3 routes per stop with the bus times for each route. stores each object for each stop in an busSchedule array
+//     function createSchedule(){
 
-    //get bus times for all 3 routes for this stop
-    //after all 3 route arrays are populated with bus times, add the routeObj as the value to that stop
-    function getStop(stop){
-      //populate times for stop 1 route 1
-      // console.log('bus schedule in getstop before for loop:', busSchedule);
-      
-      var routeObj = {};
-      for (let i = 1; i < 4; i++){
-        console.log('need to run getRoute(i)');
-        getRoute(1, stop);
-      }
-      // console.log('BUS SCHEDULE after for loop in getStop:', busSchedule[0].stop1);
-      // return res.end(JSON.stringify(busSchedule));
-    } //end getStop
 
-    createSchedule();
-    // var result = createSchedule();
-    // console.log('RESULT:', result);
-    // result.then(function(data){
-    //   return res.end(JSON.stringify(busSchedule));
-    // });
-  }
-}
+//       for (let i = 0; i < 10; i++){
 
-module.exports = scheduleController;
+//         getStop((i+1));
+//         //TODO: run getStop for each stop. need to populate an array of route1starts
+//       }
+//       return busSchedule;
+//     }
+
+//     createRoute1Starts();
+//     getStop(1);
+//     // console.log(getRoute(1, 1));
+//     // createSchedule();
+
+//     // var result = createSchedule();
+//     // console.log('RESULT:', result);
+//     // result.then(function(data){
+//     //   return res.end(JSON.stringify(busSchedule));
+//     // });
+//   }
+// }
+
+// module.exports = scheduleController;
 
 //look into moment.js for times
 
@@ -145,78 +194,128 @@ module.exports = scheduleController;
 //is time format a string??
 
 ////just in case
-// const Promise = require('bluebird');
+const Promise = require('bluebird');
 
-// const scheduleController = {
-//   getData: (req, res, next) => {
-//     let busSchedule = {};
-//     let route1start = [];
-//     function createSchedule(){
-
-
-//       for (let i = 0; i < 10; i++){
-//         busSchedule['stop' + (i+1).toString()] = {};
-//         route1start.push(i*2);
-//         getStop((i+1),route1start[i]);
-//         //TODO: run getStop for each stop. need to populate an array of route1starts
-//       }
-//       return busSchedule;
-//       console.log('NEW SECTION!!');
-//       console.log('route1start:', route1start);
-//     }
+const scheduleController = {
+  getData: (req, res, next) => {
     
+      let busSchedule = {};
+      let route1start = [];
 
-//     function getStop(stop, route1start){
-//       //populate times for stop 1 route 1
-//       function getRoute(route){
-//         var start;
-//         if (route === 1){
-//           start = route1start;
-//         } else if (route === 2){
-//           start = route1start + 2;
-//         } else {
-//           start = route1start + 4;
-//         }
+      
 
-//         busSchedule[`stop${stop}`][`route${route}`] = [];
+      function getStop(stop, route1start){
+        //populate times for stop 1 route 1
+        function getRoute(route){
+          var start;
+          if (route === 1){
+            start = route1start;
+          } else if (route === 2){
+            start = route1start + 2;
+          } else {
+            start = route1start + 4;
+          }
 
-//         for (let i = start; i < 1440; i+=15){
-//           var j;
-//           if (i < 60) {
-//             if (i < 10){
-//               j = '0' + i.toString();
-//             } else {
-//               j = i.toString();
-//             }
-//             busSchedule[`stop${stop}`][`route${route}`].push(`00:${j}`);
-//           } else {
-//             let hour = Math.floor(i / 60);
-//             if (hour < 10){
-//               hour = '0' + hour.toString();
-//             } else {
-//               hour = hour.toString();
-//             }
-//             let min = i % 60;
-//             if (min < 10){
-//               min = '0' + min.toString();
-//             } else {
-//               min = min.toString();
-//             }
-//             busSchedule[`stop${stop}`][`route${route}`].push(`${hour}:${min}`);
-//           }
-//         }
-//       }
-//       //get bus times for all 3 routes for this stop
-//       for (let i = 1; i < 4; i++){
-//         getRoute(i);
-//       }
-//       console.log('BUS SCHEDULE:', busSchedule);
-//       // return res.end(JSON.stringify(busSchedule));
-//     } //end getStop
-//     var result = createSchedule();
-//     console.log('RESULT:', result);
-//     result.then(function(data){
-//       return res.end(JSON.stringify(busSchedule));
-//     });
-//   }
-// }
+          busSchedule[`stop${stop}`][`route${route}`] = [];
+
+          for (let i = start; i < 1440; i+=15){
+            var j;
+            if (i < 60) {
+              if (i < 10){
+                j = '0' + i.toString();
+              } else {
+                j = i.toString();
+              }
+              busSchedule[`stop${stop}`][`route${route}`].push(`00:${j}`);
+            } else {
+              let hour = Math.floor(i / 60);
+              if (hour < 10){
+                hour = '0' + hour.toString();
+              } else {
+                hour = hour.toString();
+              }
+              let min = i % 60;
+              if (min < 10){
+                min = '0' + min.toString();
+              } else {
+                min = min.toString();
+              }
+              busSchedule[`stop${stop}`][`route${route}`].push(`${hour}:${min}`);
+            }
+          }
+        }
+        //get bus times for all 3 routes for this stop
+        for (let i = 1; i < 4; i++){
+          getRoute(i);
+        }
+        // console.log('BUS SCHEDULE:', busSchedule);
+        // return res.end(JSON.stringify(busSchedule));
+      } //end getStop
+      // resolve(busSchedule);
+
+    // });
+    var arrayOfPromises = [];
+    function createSchedule(){
+      return new Promise(function(resolve, reject){
+        for (let i = 0; i < 10; i++){
+          busSchedule['stop' + (i+1).toString()] = {};
+          route1start.push(i*2);
+          getStop((i+1),route1start[i]); 
+        }
+        resolve(busSchedule);
+      });
+      // for (let i = 0; i < 10; i++){
+      //   // arrayOfPromises.push(new Promise (function(resolve, reject){
+      //     busSchedule['stop' + (i+1).toString()] = {};
+      //     route1start.push(i*2);
+      //     getStop((i+1),route1start[i]);
+      //     // resolve(busSchedule);
+      //     //TODO: run getStop for each stop. need to populate an array of route1starts
+      //   // }));
+      // }
+      // return busSchedule;
+      // console.log('NEW SECTION!!');
+      // console.log('route1start:', route1start);
+    }
+    var result = createSchedule();
+    // var results = Promise.all(arrayOfPromises);
+
+    // var result = createSchedule();
+    console.log('RESULT:', result);
+    result.then(function(data){
+      console.log('DATA:', data);
+      return res.end(JSON.stringify(data));
+    });
+    
+  }
+}
+
+module.exports = scheduleController;
+
+
+//extra code:
+  // var arrayOfPromises = [];
+  // function createSchedule(){
+
+  //   for (let i = 0; i < 10; i++){
+  //     arrayOfPromises.push(new Promise (function(resolve, reject){
+  //       busSchedule['stop' + (i+1).toString()] = {};
+  //       route1start.push(i*2);
+  //       getStop((i+1),route1start[i]);
+  //       resolve(busSchedule);
+  //       //TODO: run getStop for each stop. need to populate an array of route1starts
+  //     }));
+  //   }
+  //   // return busSchedule;
+  //   // console.log('NEW SECTION!!');
+  //   // console.log('route1start:', route1start);
+  // }
+  // createSchedule();
+  // var results = Promise.all(arrayOfPromises);
+
+  // // var result = createSchedule();
+  // console.log('RESULTS:', results);
+  // results.then(function(data){
+  //   console.log('DATA:', data);
+  //   return res.end(JSON.stringify(data));
+  // });
